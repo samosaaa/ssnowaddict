99aeceb27c28d0c2e3c000165c3878124e50ee91
samosaaa <samra_adl@outlook.fr>
2022-10-25 23:20:43 +0200

    updates brought on LIVE

--- index.php
+++ index.php
@@ -2,21 +2,31 @@
     //define('HOMEPAGE_PATH','/');
     //define('CONTACT_PATH','/contact');
 
-    const HOMEPAGE_PATH = '/';
-    const CONTACT_PATH = '/contact';
+    //const HOMEPAGE_PATH = '/';
+    const CONTACT_PATH = 'contact';
 
     require_once('views/base.php');
-
+    
     var_dump($_SERVER['REQUEST_URI']);
 
-    switch($_SERVER['REQUEST_URI']) {
-        case HOMEPAGE_PATH; //case '/';
+    $requestUri = explode('/',$_SERVER['REQUEST_URI']); // un tableau contenant le chemin
+
+    switch ($requestUri[array_key_last($requestUri)]) {
+        /**case HOMEPAGE_PATH; //case '/';
             echo 'ACCEUIL';
-            break;
-        case CONTACT //case '/contact':
+            break;*/
+        case CONTACT_PATH: //case '/contact':
+            $controlleur = 'contact';
             echo 'CONTACT';
             break;
+        default:
+            $controlleur = 'accueil';
+            echo 'ACCUEIL';
+            break;
+
     }
+
+    require_once('controllers/'.$controlleur.'Controller.php');
 
     /*
         if ($_SERVER['REQUEST_URI'] === '/') { // car les dossiers se termine par des / slash
