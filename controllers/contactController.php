d70d54d189ea0e2f6f082206d9853b4376881697
samosaaa <samra_adl@outlook.fr>
2022-10-26 10:48:14 +0200

    Day3 Class et Objet

--- controllers/contactController.php
+++ controllers/contactController.php
@@ -1,3 +1,15 @@
 <?php
     echo 'Je suis le controlleur contact';
 ?>
+
+<?php
+    class ContactController {
+
+        /**
+         * Contact function
+         */
+        function contact( String $name): string {
+            return "HEllo $name!";
+        }
+    }
+?>
--- controllers/homepageController.php
+++ controllers/homepageController.php
@@ -1,3 +1,12 @@
 <?php
     echo 'Je suis le controlleur home';
 ?>
+
+<?php
+    class HomepageController {
+
+        function home($name): string {
+            return 'JE suis sur la page d\'accueil du contrôleur homepage';
+        }
+    }
+?>
